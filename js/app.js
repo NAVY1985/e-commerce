4c0ca26cc994ccccfb348bb0d354673bdfc2faf5
navy <jezreelsanz@gmail.com>
2018-08-07 00:44:34 -0500

    se agregan estilos y se modifica funcionalidad

--- js/app.js
+++ js/app.js
@@ -3,11 +3,14 @@ var form = $('#search-form');
 var search = $('#searchProducts');
 var searchedForText;
 
-form.submit(function(ev) {
+form.click(function(ev) {
     ev.preventDefault();
     searchedForText = search.val();
     $('#searchProducts').val("");
-
+    if (searchedForText.length !== 0) {
+        $('#content').empty();
+        $('#tittle').empty();
+    }
     getData();
 });
 
@@ -19,32 +22,27 @@ function getData() {
         crossDomain: true,
         success: function(response) {
             console.log(response);
+
             var tempTittle = `<h3>${searchedForText}</h3>`
             $('#tittle').append(tempTittle);
 
-            for (var i = 0; i <= 5; i++) {
+            for (var i = 0; i <= 8; i++) {
                 var photo = response.results[i].thumbnail;
                 var titleProduct = response.results[i].title;
                 var priceProduct = response.results[i].price;
 
 
-                var template = `<div class="card mt-4" style="width: 16rem;">
+                var template = `<div class="card mt-4" style="width: 18rem;">
                          <div class="card-body text-center">
-                             <img class="card-img-top" src="${photo}" alt="Card image cap">
+                             <img class="card-img-top img-fluid" src="${photo}" alt="Card image cap">
                              <h5 class="card-title mt-2">${titleProduct}</h5>
                              <p class="card-text"><strong>Precio: $ ${priceProduct}</strong></p>
-                             <a href="#" class="btn btn-primary">Add To Car</a>
+                             <a href="#" class="btn btn-success">Add To Car</a>
                          </div>
                       </div>`;
 
-
                 $('#content').append(template);
-
-
             };
-
         }
     });
-
-
 };
